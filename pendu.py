29a77a78c15d3c37546f913713eb49234b679650
Benjamin Potron <benjaminpotron@gmail.com>
2022-10-23 18:03:41 +0200

    Update pendu.py from pendu_exe.py

--- pendu.py
+++ pendu.py
@@ -108,49 +108,50 @@ def pendu():
         lettre=input("Saississez une lettre :")
         if lettre in lettres_utilisees:
             print("Tu as déjà utilisé cette lettre !")
-        if lettre == '':
-            print("Veuillez saisir une lettre.")
-        if lettre not in lettre_maj and lettre not in lettre_min:
-            print("Veuillez saisir un caractère valide. Dommage pour vous !")
-        elif lettre not in lettres_utilisees:
-            if lettre not in lettre_min:
-                lettres_utilisees.append(lettre)
-                for i in range(len(lettre_maj)):
-                    if chr(i+65)==lettre:
-                        lettres_utilisees.append(chr(i+97))
-            else:
-                lettres_utilisees.append(lettre)
+        else :
+            if lettre == '':
+                print("Veuillez saisir une lettre.")
+            if lettre not in lettre_maj and lettre not in lettre_min:
+                print("Veuillez saisir un caractère valide. Dommage pour vous !")
+            elif lettre not in lettres_utilisees:
+                if lettre not in lettre_min:
+                    lettres_utilisees.append(lettre)
+                    for i in range(len(lettre_maj)):
+                        if chr(i+65)==lettre:
+                            lettres_utilisees.append(chr(i+97))
+                else:
+                    lettres_utilisees.append(lettre)
+                    for i in range(len(lettre_min)):
+                        if chr(i+97)==lettre:
+                            lettres_utilisees.append(chr(i+65))
+            if lettre in lettre_min:
                 for i in range(len(lettre_min)):
-                    if chr(i+97)==lettre:
-                        lettres_utilisees.append(chr(i+65))
-        if lettre in lettre_min:
-            for i in range(len(lettre_min)):
-                if lettre_min[i]==lettre:
-                    lettre = chr(i+65)
-        # print(lettres_utilisees)
-        if lettre in mot_choisi:
-            for i in range(len(mot_choisi)):
-                if mot_choisi[i]==lettre:
-                    mot_cache[i]=lettre
-                    #print(mot_cache)
-                    if "_" not in mot_cache:
-                        score += 1
-                        print("Félicitations, vous avez gagné ! Le mot à deviner était " + mot_choisi + ".")
-                        print("Votre score est désormais de " + str(score) + ".")
-                        restart()
-                        #exit()
-            # print("ok")
-        else:
-            print(dessinPendu(nb_erreurs))
-            nb_erreurs=nb_erreurs+1
-            if nb_erreurs==7:
-                print("Vous avez perdu ! Le mot à trouver était : " + mot_choisi + ". Réessayez et peut-être que vous réussirez !")
-                print("Votre score était de " + str(score) + ". Il a été maintenant réinitialisé à 0.")
-                score = 0
-                restart()
-        for i in range(len(mot_cache)-1):
-            print(mot_cache[i], end="")
-            print(" ", end="")
-        print(mot_cache[-1])
+                    if lettre_min[i]==lettre:
+                        lettre = chr(i+65)
+            # print(lettres_utilisees)
+            if lettre in mot_choisi:
+                for i in range(len(mot_choisi)):
+                    if mot_choisi[i]==lettre:
+                        mot_cache[i]=lettre
+                        #print(mot_cache)
+                        if "_" not in mot_cache:
+                            score += 1
+                            print("Félicitations, vous avez gagné ! Le mot à deviner était " + mot_choisi + ".")
+                            print("Votre score est désormais de " + str(score) + ".")
+                            restart()
+                            #exit()
+                # print("ok")
+            else:
+                print(dessinPendu(nb_erreurs))
+                nb_erreurs=nb_erreurs+1
+                if nb_erreurs==7:
+                    print("Vous avez perdu ! Le mot à trouver était : " + mot_choisi + ". Réessayez et peut-être que vous réussirez !")
+                    print("Votre score était de " + str(score) + ". Il a été maintenant réinitialisé à 0.")
+                    score = 0
+                    restart()
+            for i in range(len(mot_cache)-1):
+                print(mot_cache[i], end="")
+                print(" ", end="")
+            print(mot_cache[-1])
 
 pendu()
